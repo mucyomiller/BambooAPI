43ac05ec888be013b1a86b2652840066851d72ba
Mucyo F Miller <mucyomiller@gmail.com>
2017-02-27 13:37:02 +0200

    change web structure

---
+++ server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/engine/standard"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	e := echo.New()
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	//CORS
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
+	}))
+	// Server
+	e.Run(standard.New(":8080"))
+}
