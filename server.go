3c310fc4c35948c53b3d98d6e65bd2ae5129d4c3
Mucyo F Miller <mucyomiller@gmail.com>
2017-05-03 13:46:55 +0200

    updated changes to application entry

--- server.go
+++ server.go
@@ -3,15 +3,14 @@ package main
 import (
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	//"github.com/mucyomiller/BambooAPI/controllers"
-	"github.com/mucyomiller/BambooAPI/views"
+	"github.com/mucyomiller/BambooAPI/controllers"
 )
 
 func main() {
 
 	e := echo.New()
 	e.Debug = true
-	e.Renderer = views.T
+	e.Renderer = controllers.T
 	// Middleware
 	// logging request info
 	e.Use(middleware.Logger())
@@ -21,8 +20,9 @@ func main() {
 	e.Use(middleware.Gzip())
 
 	e.Static("/static", "assets")
-	e.GET("/", views.Index)
-	e.GET("/home", views.Home)
+	e.GET("/", controllers.Index)
+	e.GET("/home", controllers.Home)
+	e.GET("/create", controllers.Create)
 	//e.GET("/favicon.ico", echo.WrapHandler(http.NotFoundHandler()))
 	e.Logger.Fatal(e.Start(":8080"))
 }
